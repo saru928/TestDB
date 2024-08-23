d3518f25c4b2e926a150b5e4893013595a4ebc2d
saru928 <kana.softball5@gmail.com>
2024-08-23 21:45:51 +0900

    実行成功

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -6,15 +6,21 @@ import java.sql.SQLException;
 public class DBConnector {
 
 	private static String driverName = "com.mysql.jdbc.Driver";
+	// JDBCのドライバー (Javaとデータベースを繋げる箱)の名前を変数に代入
 	
 	private static String url = "jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+	//URLの指定　localhost(自分のPC)testdb(データベース名)を使用
+	//？以降はオプションのため必須ではない
 	
 	private static String user = "root";
 	
-	private static String password = "mysql";
+	private static String password = "root";
+	//パスワードは自分の環境に合ってるか確認する
 	
 	public Connection getConnection() {
 		Connection con = null;
+		//接続状態にする
+		//一度状態を初期化
 		
 		try {
 			Class.forName(driverName);
--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -11,14 +11,18 @@ public class TestUserDAO {
 	public void select(String name,String password) {
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
+		//DBへの接続準備
 		
 		String sql = "select * from test_table where user_name=? and password=?";
+		//?はプレースホルダー。都度違うデータを入れたい際に使用
 		
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString(1,  name);
 			ps.setString(2, password);
 			ResultSet rs = ps.executeQuery();
+			//PreparedStatementとは DBまで運んでくれる箱
+			//executeQuery();は実行メソッド。必ずResultSetが返ってくる
 			if(rs.next()) {
 				System.out.println(rs.getString("user_name"));
 				System.out.println(rs.getString("password"));
@@ -27,6 +31,8 @@ public class TestUserDAO {
 			e.printStackTrace();
 		}try {
 			con.close();
+			//データベースを終了させるという意味
+			//これがないとデータベースに接続したまま次の作業が実行され、メモリに負荷がかかる
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
